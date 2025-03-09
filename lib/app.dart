7f1c5816659c8af230de8674bc986cc6470d7bf4
clementGilardy <clement.gilardy@outlook.fr>
2025-03-09 10:29:41 +0100

    feat: update import

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:toi_et_moi/theme/app_theme.dart';
+import 'package:fair_share/theme/app_theme.dart';
 import 'screens/home/fair_share_home_page.dart';
 import 'screens/settings/fair_share_settings_page.dart';
 
